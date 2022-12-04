ac35a81dc78108981eb89c1be4f72ff8464e1ad4
ipelu <ipeluaga@gmail.com>
2022-12-04 02:19:41 +0100

    Updated

--- arduino_ldr/arduino_ldr.ino
+++ arduino_ldr/arduino_ldr.ino
@@ -13,7 +13,6 @@ void loop() {
   while (Serial.available() <= 0){} // Espera a recibir algo por el puerto serie
     if (Serial.read()== 'r') {
       sensorValue = analogRead(ldrPin);
-      //sensorValueEscalado = (sensorValue * 100 )/ 1023;
       sensorValueEscalado = sensorValue / 10.23;
       Serial.println(sensorValueEscalado);
     }
